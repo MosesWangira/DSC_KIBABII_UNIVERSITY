1d8526ed4647925b5eefe0ee9b5fe30025185c2f
jamesnyakush <emvesttech@gmail.com>
2020-01-17 12:45:37 +0300

    added android adapter and model

---
+++ Android/src/main/java/com/dsckibu/android/data/adapter/AndroidAdapter.kt
@@ -0,0 +1,24 @@
+package com.dsckibu.android.data.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class AndroidAdapter  : RecyclerView.Adapter<AndroidAdapter.AndroidViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AndroidViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: AndroidViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    class AndroidViewHolder(val  view : View) : RecyclerView.ViewHolder(view)
+
+
+}
